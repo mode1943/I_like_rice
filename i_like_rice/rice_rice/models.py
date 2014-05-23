19c09a225df8879108a1cba20e4bdab4e5b40ead
root <root@ubuntu-hbl.(none)>
2014-05-23 16:49:40 +0800

    add type

--- i_like_rice/rice_rice/models.py
+++ i_like_rice/rice_rice/models.py
@@ -2,14 +2,32 @@ from datetime import datetime
 from time import time
 from PIL import Image
 
-
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 # Create your models here.
+
+rice_CHOICES = (
+                ('1', _('rice with meat and vegetables')),
+                ('2', _('pasta')),
+                ('3', _('a fried dish')),
+                ('4', _('drink')),
+                ('5', _('rice flour')),
+                )
+
+priority_CHOICES = (
+                    ('A', _('five rice')),
+                    ('B', _('four rice')),
+                    ('C', _('three rice')),
+                    ('D', _('two rice')),
+                    ('E', _('zero rice')),
+                    )
+
 class RiceRoom(models.Model):
     """ define the restaurant"""
     name = models.CharField(max_length=50, blank=True, null=True, verbose_name='restanrant')
     telephone = models.CharField(max_length=15, blank=True, null=True, verbose_name='tel')
+    priority = models.CharField(max_length=5, default='E', choices=priority_CHOICES, verbose_name='the ranking')
     is_open = models.IntegerField(max_length=1, default=1, verbose_name='wether on the bussiness')
     c_time = models.DateTimeField(auto_now_add=True)
     def __unicode__(self):
@@ -38,10 +56,11 @@ class Rice(models.Model) :
     """define the cookbook """
     name = models.CharField(max_length=20, blank=True, null=True, verbose_name='dish' )
     price = models.DecimalField(max_digits=8, decimal_places=3, default=0.00, verbose_name='price')
+    rice_type = models.CharField(max_length=5, default='1', choices=rice_CHOICES, verbose_name='rice_type')
     riceroom = models.ForeignKey(RiceRoom, null=True)
     description = models.CharField(max_length=100, blank=True, null=True, verbose_name='desc')
     total_rices = models.IntegerField(max_length=10, blank=True, null=True, default=100, verbose_name='total_nums')
-    current_rices = models.IntegerField(max_length=10, blank=True, null=True, default=0, verbose_name='current_time')
+    current_rices = models.IntegerField(max_length=10, blank=True, null=True, default=0, verbose_name='current_nums')
     is_sell = models.CharField(max_length=1, default='1')
     c_time = models.DateField(auto_now_add=True)
     
@@ -77,6 +96,7 @@ class Rice(models.Model) :
         else:
             raise ValueError('you should give me a rice')
 
+
 class RicePic(models.Model):
     """ define the rice dish picture"""
     name = models.CharField(max_length=20, blank=True, null=True, verbose_name='name')
@@ -94,13 +114,13 @@ class RicePic(models.Model):
         from django.conf import settings
         import os
         if f is not None and Rice.objects.filter(pk=int(rice_id)).exists():
-            s_path = "/%s/%s/%s/" %(datetime.now().year, datetime.now().month, datetime.now().day)
+            s_path = "%s/%s/%s/" %(datetime.now().year, datetime.now().month, datetime.now().day)
             save_path = os.path.join(settings.PHOTO_ROOT, s_path)
-            save_name = "%s" % int(time())
+            save_name = "%s.jpg" % int(time())
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
-            url_path = os.path.join(settings.PHOTO_URL, s_path+save_name)
-            cls.objects.create(name=f.name, path=url_path, rice_id=rice_id)
+            url_path = os.path.join(settings.PHOTO_URL, s_path, save_name)
+            cls.objects.create(name=os.path.splitext(f.name)[0], path=url_path, rice_id=rice_id)
             img = Image.open(f)
             img.thumbnail((150,210), Image.ANTIALIAS)
             img.save(os.path.join(save_path, save_name), quality=100)
@@ -120,13 +140,3 @@ class RicePic(models.Model):
                 raise ValueError('the pic does not exists')
         else:
             raise ValueError('the picture you want to delete cannot be null')
-     
-
-
-            
-
-            
-            
-        
-
-
